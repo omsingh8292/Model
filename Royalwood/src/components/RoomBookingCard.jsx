3b9ad0058301270a827c93eaf161716691653ec3
Omprakash Kumar Singh <kamalrajput99550@gmail.com>
2026-03-09 21:19:36 +0530

    polpol

--- Royalwood/src/components/RoomBookingCard.jsx
+++ Royalwood/src/components/RoomBookingCard.jsx
@@ -90,6 +90,7 @@ Reserve Room
 </div>
 // all things are in the card, so we close the card div here
 // all things are in the card, so we close the card div here
+//lolo the dat are o good that all capture through the mainImage
 </div>
 
 )
