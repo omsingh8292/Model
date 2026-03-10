0a0e20511f222a4cb632d40e7182e13a0d6ee945
Omprakash Kumar Singh <kamalrajput99550@gmail.com>
2026-03-10 23:17:02 +0530

    tata

--- Royalwood/src/components/RoomBookingCard.jsx
+++ Royalwood/src/components/RoomBookingCard.jsx
@@ -91,6 +91,16 @@ Reserve Room
 // all things are in the card, so we close the card div here
 // all things are in the card, so we close the card div here
 //lolo the dat are o good that all capture through the mainImage
+// all things are in the card, so we close the card div here
+// all things are in the card, so we close the card div here
+//lolo the dat are o good that all capture through the mainImage
+// all things are in the card, so we close the card div here
+// all things are in the card, so we close the card div here
+//lolo the dat are o good that all capture through the mainImage
+// all things are in the card, so we close the card div here
+// all things are in the card, so we close the card div here
+//lolo the dat are o good that all capture through the mainImage
+
 </div>
 
 )
